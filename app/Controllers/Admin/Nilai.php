220b42b5f0eb53e9ec156313db78d2093a627c09
randyesa17 <randyesa17@gmail.com>
2021-08-19 19:18:11 +0700

    Update Tampilan Admin

--- app/Controllers/Admin/Nilai.php
+++ app/Controllers/Admin/Nilai.php
@@ -4,6 +4,7 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Models\NilaiModel;
+use App\Models\RuangKelasModel;
 use App\Models\MapelModel;
 use App\Models\JurusanModel;
 
@@ -12,15 +13,18 @@ class Nilai extends BaseController
     public function index()
     {
         $model = new NilaiModel();
+        $modelRuang = new RuangKelasModel();
         $modelMapel = new MapelModel();
         // $modelJurusan = new JurusanModel();
 
         $nilai = $model->findAll();
+        $ruang = $modelRuang->findAll();
         $mapel = $modelMapel->findAll();
         // $jurusan = $modelJurusan->findAll();
         $data = [
             'judul' => 'Daftar Nilai',
             'nilai' => $nilai,
+            'ruang' => $ruang,
             'mapel' => $mapel,
             // 'jurusan' => $jurusan,
         ];
@@ -31,15 +35,25 @@ class Nilai extends BaseController
     {
         $model = new NilaiModel();
         $modelMapel = new MapelModel();
+        $modelRuang = new RuangKelasModel();
         // $modelJurusan = new JurusanModel();
 
-        $nilai = $model->where('idmapel', $_GET['idmapel'])->findAll();
+        $ruang = $modelRuang->where('koderuang', $_GET['koderuang'])->findAll();
+        $nama=$ruang[0]['namaruang'];
+
+        foreach ($ruang as $key => $value) {
+            $nilai[$key] = $model->where('nis', $value['nis'])->first();
+        }
+        
         $mapel = $modelMapel->findAll();
+        $ruang = $modelRuang->findAll();
         // $jurusan = $modelJurusan->findAll();
         $data = [
-            'judul' => 'Daftar Nilai',
+            'judul' => 'Daftar Nilai '.$nama,
             'nilai' => $nilai,
             'mapel' => $mapel,
+            'ruang' => $ruang,
+            'kode' => $_GET['koderuang'],
         ];
         return view('admin/nilai', $data);
         // echo $_GET['idmapel'];
@@ -47,10 +61,14 @@ class Nilai extends BaseController
 
     public function rekap()
     {
-        if (!empty($_GET['idmapel'])) {
+        if (!empty($_GET['koderuang'])) {
             $model = new NilaiModel();
 
-            $nilai = $model->where('idmapel', $_GET['idmapel'])->findAll();
+            $ruang = $modelRuang->where('koderuang', $_GET['koderuang'])->findAll();
+
+            foreach ($ruang as $key => $value) {
+                $nilai[$key] = $model->where('nis', $value['nis'])->first();
+            }
 
             foreach ($nilai as $key => $value) {
                 $nTugas = $value['nilaiTugas'];
@@ -60,8 +78,8 @@ class Nilai extends BaseController
                 $data = [
                     'nilaiAkhir' => $nAkhir,
                 ];
-                $model->where('idmapel', $_GET['idmapel'])->set($data)->update();
-                return redirect()->to(site_url('admin/nilai/cari?idmapel='.$_GET['idmapel']));
+                $model->where('idmapel', $value['idmapel'])->set($data)->update();
+                return redirect()->to(site_url('admin/nilai/cari?koderuang='.$_GET['koderuang']));
             }
         } else {
             $model = new NilaiModel();
--- app/Views/admin/nilai.php
+++ app/Views/admin/nilai.php
@@ -1,75 +1,128 @@
 <?= $this->extend('templates/admin') ?>
 <?= $this->section('content') ?>
-<h1><?= $judul ?></h1>
-<hr>
-<form action="<?= site_url('admin/nilai/cari') ?>" method="get">
-    <div class="form-group">
-        <select class="form-control" onchange="this.form.submit()" name="idmapel" id="idmapel">
-            <option value="-1">--Pilih Mata Pelajaran--</option>
-            <?php foreach($mapel as $key => $value) : ?>
-            <option value="<?= $value['idmapel'] ?>"><?= $value['mapel'] ?></option>
-            <?php endforeach; ?>
-        </select>
+<style>
+@media print {
+    body * {
+        visibility: hidden;
+    }
+
+    #section-to-print,
+    #section-to-print * {
+        visibility: visible;
+    }
+
+    #section-to-print {
+        position: absolute;
+        left: 0;
+        top: 0;
+    }
+
+    #print {
+        visibility: hidden;
+    }
+
+    #koderuang {
+        visibility: hidden;
+    }
+
+    #hid {
+        visibility: hidden;
+    }
+}
+</style>
+<div class="pd-ltr-20 xs-pd-20-10" id="section-to-print">
+    <div class="min-height-200px">
+        <div class="page-header">
+            <div class="row">
+                <div class="col-md-6 col-sm-12">
+                    <div class="title">
+                        <h4><?= $judul ?></h4>
+                    </div>
+                    <nav aria-label="breadcrumb" role="navigation">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="<?= site_url('admin/home') ?>">Beranda</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Nilai</li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="col-md-6 col-sm-12 text-right">
+                    <form action="<?= site_url('admin/nilai/cari') ?>" method="get">
+                        <select class="form-control btn" onchange="this.form.submit()" name="koderuang" id="koderuang">
+                            <option value="-1">--Pilih Ruang Kelas--</option>
+                            <?php foreach($ruang as $key => $value) : ?>
+                            <option value="<?= $value['koderuang'] ?>"><?= $value['namaruang'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="pd-20 card-box mb-30">
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <tr>
+                        <th width="5%">No</th>
+                        <th>NIS</th>
+                        <th>Mata Pelajaran</th>
+                        <th>Nilai Tugas</th>
+                        <th>Nilai Ujian</th>
+                        <th>Nilai Akhir</th>
+                    </tr>
+                    <?php $no = 1;
+                    foreach ($nilai as $key => $value) : ?>
+                    <tr>
+                        <td><?= $no++ ?></td>
+                        <td><?= $value['nis'] ?></td>
+                        <td>
+                            <?php
+                                foreach($mapel as $key1 => $value1){
+                                    if ($value['idmapel'] == $value1['idmapel']) {
+                                        echo $value1['mapel'];
+                                    }
+                                }
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                if ($value['nilaiTugas'] != "") {
+                                    echo $value['nilaiTugas'];
+                                } else {
+                                    echo "Belum Ada";
+                                }
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                if ($value['nilaiUjian'] != "") {
+                                    echo $value['nilaiUjian'];
+                                } else {
+                                    echo "Belum Ada";
+                                }
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                if ($value['nilaiAkhir'] != "") {
+                                    echo $value['nilaiAkhir'];
+                                } else {
+                                    echo "Belum Ada";
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+            <?php if(!empty($nilai)) : ?>
+            <?php if(empty($_GET['idmapel'])) : ?>
+            <a href="<?= site_url('admin/nilai/rekap') ?>" id="hid" class="btn btn-primary">Rekap Semua Data</a>
+            <?php else : ?>
+            <a href="<?= site_url('admin/nilai/rekap?koderuang='.$kode) ?>" id="hid" class="btn btn-primary">Rekap Semua
+                Data</a>
+            <?php endif; ?>
+            <?php endif; ?>
+            <input id="print" class="btn btn-primary pull-right" onclick="window.print()" value="PRINT">
+        </div>
     </div>
-</form>
-<table class="table table-success">
-    <tr>
-        <th>No</th>
-        <th>NIS</th>
-        <th>Mata Pelajaran</th>
-        <th>Nilai Tugas</th>
-        <th>Nilai Ujian</th>
-        <th>Nilai Akhir</th>
-    </tr>
-    <?php $no = 1; foreach($nilai as $key => $value) : ?>
-    <tr>
-        <td><?= $no++ ?></td>
-        <td><?= $value['nis'] ?></td>
-        <td>
-            <?php
-        foreach($mapel as $key1 => $value1){
-            if ($value['idmapel'] == $value1['idmapel']) {
-                echo $value1['mapel'];
-            }
-        }
-        ?>
-        </td>
-        <td>
-            <?php
-        if ($value['nilaiTugas'] != "") {
-            echo $value['nilaiTugas'];
-        } else {
-            echo "Belum Ada";
-        }
-        ?>
-        </td>
-        <td>
-            <?php
-        if ($value['nilaiUjian'] != "") {
-            echo $value['nilaiUjian'];
-        } else {
-            echo "Belum Ada";
-        }
-        ?>
-        </td>
-        <td>
-            <?php
-        if ($value['nilaiAkhir'] != "") {
-            echo $value['nilaiAkhir'];
-        } else {
-            echo "Belum Ada";
-        }
-        ?>
-        </td>
-    </tr>
-    <?php endforeach; ?>
-</table>
-<?php if(!empty($nilai)) : ?>
-<?php if(empty($_GET['idmapel'])) : ?>
-<a href="<?= site_url('admin/nilai/rekap') ?>" class="btn btn-primary">Rekap Semua Data</a>
-<?php else : ?>
-<a href="<?= site_url('admin/nilai/rekap?idmapel='.$_GET['idmapel']) ?>" class="btn btn-primary">Rekap Semua Data</a>
-<?php endif; ?>
-<?php endif; ?>
-<input class="btn btn-primary pull-right" onclick="window.print()" value="PRINT">
+</div>
 <?= $this->endSection() ?>
